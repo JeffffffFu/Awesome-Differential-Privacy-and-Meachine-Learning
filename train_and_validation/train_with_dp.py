d08e2b147177fb7ab0f5b48c9cce2c8b0800eb95
jie.Fu1 <Jie.Fu1@geely.com>
2023-01-12 18:58:55 +0800

    opacus

--- train_and_validation/train_with_dp.py
+++ train_and_validation/train_with_dp.py
@@ -7,20 +7,12 @@ import torch.nn.functional as F
 #这里实际上做的也是小批量梯度下降，在裁剪上是逐样本，为了更好的控制DPSGD，这边的train_loader我们希望只有一个batch就好，多个也可以
 def train_dynamic_add_noise(model, train_loader, optimizer):
     model.train()
-    microbatch_loss=0.0
     train_loss = 0.0
-    correct = 0
-    group=0
-    output_batch=[]
     aa=0
     train_acc=0.
     i=0
     for id,(data, target) in enumerate(train_loader):  # batch之前组装到data数据集里的,pytorch的MBDG统一用这种方式进行,会按序列一个个batch训练
-        #print("id:",id)
         optimizer.zero_accum_grad()  # 梯度清空
-
-        #这里执行单样本操作，但是没有参数决定是单样本，依赖这里面的数据集的组装形式（TensorDataset(data, target)），和上面的train_loader一样，默认都是一个torch一个torch来
-        # 数据组装中torch的维度决定你想要进行多少样本的梯度训练，取决于一开始的数据组装的结构
         for iid,(X_microbatch, y_microbatch) in enumerate(TensorDataset(data, target)):  #这里相当于逐样本
 
             optimizer.zero_microbatch_grad()
@@ -28,8 +20,7 @@ def train_dynamic_add_noise(model, train_loader, optimizer):
             loss = F.cross_entropy(output, torch.unsqueeze(y_microbatch, 0))
 
             loss.backward()         #梯度求导，这边求出梯度
-            norm=optimizer.microbatch_step()  # 这个step做的是每个样本的梯度裁剪和梯度累加的操作
-            # print("norm:",norm)
+            optimizer.microbatch_step()  # 这个step做的是每个样本的梯度裁剪和梯度累加的操作
         optimizer.step_dp()  # 这个做的是梯度加噪和梯度平均更新下降的操作
 
         #训练集测试损失值和准确率
